465f1bf949006e2bda2ffaab11b44604840a106e
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2026-06-08 13:34:04 -0500

    feat(monitoring): POST /alerts/{id}/acknowledgements

--- src/main/java/com/techtitans/infratrack/platform/monitoring/interfaces/rest/AlertsController.java
+++ src/main/java/com/techtitans/infratrack/platform/monitoring/interfaces/rest/AlertsController.java
@@ -2,11 +2,13 @@ package com.techtitans.infratrack.platform.monitoring.interfaces.rest;
 
 import com.techtitans.infratrack.platform.monitoring.application.commandservices.FleetAlertCommandService;
 import com.techtitans.infratrack.platform.monitoring.application.queryservices.FleetAlertQueryService;
+import com.techtitans.infratrack.platform.monitoring.domain.model.commands.AcknowledgeFleetAlertCommand;
 import com.techtitans.infratrack.platform.monitoring.domain.model.queries.GetAllFleetAlertsQuery;
 import com.techtitans.infratrack.platform.monitoring.domain.model.queries.GetFleetAlertByIdQuery;
 import com.techtitans.infratrack.platform.monitoring.interfaces.rest.resources.AlertResource;
 import com.techtitans.infratrack.platform.monitoring.interfaces.rest.resources.CreateAlertResource;
 import com.techtitans.infratrack.platform.monitoring.interfaces.rest.transform.MonitoringResourceFromEntityAssembler;
+import com.techtitans.infratrack.platform.shared.interfaces.rest.resources.MessageResource;
 import com.techtitans.infratrack.platform.shared.interfaces.rest.transform.ResponseEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
@@ -55,6 +57,17 @@ public class AlertsController {
                 result,
                 MonitoringResourceFromEntityAssembler::toAlertResourceFromEntity,
                 HttpStatus.CREATED
+        );
+    }
+
+    @PostMapping("/{alertId}/acknowledgements")
+    public ResponseEntity<?> acknowledgeAlert(@PathVariable Long alertId) {
+        var result = fleetAlertCommandService.handle(new AcknowledgeFleetAlertCommand(alertId))
+                .map(alert -> new MessageResource("Alert acknowledged successfully"));
+        return ResponseEntityAssembler.toResponseEntityFromResult(
+                result,
+                message -> message,
+                HttpStatus.OK
         );
     }
 }
